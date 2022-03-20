c945dc27ecb0c41d089de98bdc03ec3bd2985abe
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-20 17:58:29 -0500

    fix: fixed defineConfig type

--- library/index.js
+++ library/index.js
@@ -4,6 +4,9 @@ const { vite } = require('./vite');
 const { vitest } = require('./vitest');
 const { deepMerge } = require('../utilities/deep-merge');
 
+/**
+ * @type {import('vite').defineConfig}
+ */
 module.exports.defineConfig = function defineConfig(userConfig) {
   return defineViteConfig(deepMerge(vite, vitest, userConfig));
 };
